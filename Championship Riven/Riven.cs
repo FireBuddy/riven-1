5532e392e29dce0ed8376c626fb781c644cd7fbb
AkumaCORE <k-sintu@hotmail.com>
2016-05-30 18:32:00 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -446,7 +446,7 @@ namespace Championship_Riven
 
                 if (W.IsReady() && RivenMenu.CheckBox(RivenMenu.Combo, "UseWCombo"))
                 {
-                    if (Target.IsValidTarget(W.Range))
+                    if (Target.IsValidTarget(W.Range) && !Target.CanMove)
                     {
                         Core.DelayAction(() => W.Cast(), 40);
                     }
