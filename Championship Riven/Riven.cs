309b487bce7a7708df2efb6e7d5312801423fdf5
AkumaCORE <k-sintu@hotmail.com>
2016-05-31 11:13:26 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -73,10 +73,10 @@ namespace Championship_Riven
         {
             if (ObjectManager.Player.Level <= 1)
             {
-                var allyinrange = EntityManager.Heroes.Allies.Count(h => h != null && !h.IsDead && h.Distance(Player.Instance) < 700)
+                
                 var lasthitmode = Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear);
 
-                if (!lasthitmode)
+                if (!lasthitmode || RivenMenu.CheckBox(RivenMenu.Jungleclear, "UseWJG") )
                 {
                     return;
                 }
