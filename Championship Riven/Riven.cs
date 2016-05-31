25a4e8743b34cae360411efa2b0c649db2d2ca59
AkumaCORE <k-sintu@hotmail.com>
2016-05-31 09:57:35 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -522,14 +522,14 @@ namespace Championship_Riven
                             {
                                 if (jungleMobs == null)
                                 {
-                                    Orbwalker.DisableAttacking = false
+                                    
                                     return;
                                 }
-                                if (jungleMobs !== null)
+                                if (jungleMobs != null)
                                 {
                                         if (jungleMobs.Name == "SRU_RedMini10.1.3")
                                         {
-                                                Orbwalker.DisableAttacking = true
+                                                
                                                 Player.IssueOrder(GameObjectOrder.AttackUnit, jungleMobs);
                                         }
                                         else
