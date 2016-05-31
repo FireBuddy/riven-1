66ed7d5ca729aa4c9ed45f21cc6e55aa026eff10
AkumaCORE <k-sintu@hotmail.com>
2016-05-31 08:45:54 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -72,6 +72,18 @@ namespace Championship_Riven
         public static void WallDash()
         {
         var jminions = EntityManager.MinionsAndMonsters.GetJungleMonsters(Player.Instance.ServerPosition, 1000, true);
+        foreach (var jungleMobs in jminions.Where(x => x.IsValidTarget(Player.Instance.AttackRange)))              
+                {
+                    if (jungleMobs == null)
+                    {
+                        return;
+                    }
+                    if (jungleMobs.BaseSkinName == "SRU_Blue")
+                    {
+                        E.Cast(jungleMobs);
+                    }
+                }
+            
         }    
 
         private static bool HasHydra()
