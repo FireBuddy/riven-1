a2faf743c4f05d85311699afb570cfb1920cfdf6
AkumaCORE <k-sintu@hotmail.com>
2016-05-31 08:54:29 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -514,7 +514,7 @@ namespace Championship_Riven
                 }
             }
             {
-            if (player.Level =< 1 )
+            if (Player.Level >= 1 )
                 {
                 var jminions = EntityManager.MinionsAndMonsters.GetJungleMonsters(Player.Instance.ServerPosition, 1000, true);
                 foreach (var jungleMobs in jminions.Where(x => x.IsValidTarget(Player.Instance.AttackRange)))              
