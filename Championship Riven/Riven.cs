f733ce777d0e78f26a34de4292a1203810483e77
AkumaCORE <k-sintu@hotmail.com>
2016-05-31 20:55:43 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -402,7 +402,7 @@ namespace Championship_Riven
 
                     if (FocusTarget.IsValidTarget(R2.Range))
                     {
-                        if (RDamage(FocusTarget, FocusTarget.Health) * 0.95 >= FocusTarget.Health)
+                        if (RDamage(FocusTarget, FocusTarget.Health) * 0.90 >= FocusTarget.Health)
                         {
                             var RPred = R2.GetPrediction(FocusTarget);
 
