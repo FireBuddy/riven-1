965d643498d7eeaad46f48417f246ecc7bb530d4
AkumaCORE <k-sintu@hotmail.com>
2016-06-02 00:42:58 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -578,7 +578,7 @@ namespace Championship_Riven
         private static void LastHit()
         {
             var Minion = EntityManager.MinionsAndMonsters.EnemyMinions.Where(x => x.IsValid && !x.IsDead);
-            var Minions = EntityManager.MinionsAndMonsters.GetCircularFarmLocation(Minion, Q.Width, (int)Q.Range);
+            var Minions = EntityManager.MinionsAndMonsters.GetBestCircularFarmLocation(Minion, Q.Width, (int)Q.Range);
 
             if (Minion == null)
                 return;
