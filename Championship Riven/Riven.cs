19d07a6c158293b74284ed2bf6de9e9c784cf2b5
AkumaCORE <k-sintu@hotmail.com>
2016-06-02 00:46:08 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -587,7 +587,7 @@ namespace Championship_Riven
             {
                 if (Minions.HitNumber >= RivenMenu.Slider(RivenMenu.Laneclear, "UseWLaneMin"))
                 {
-                   Q.Cast(Minions.Instance); 
+                   Q.Cast(); 
                 }
             }
 
