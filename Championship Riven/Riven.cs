de6fe9f435cfbf4c18adb31e18873c48f8d99984
AkumaCORE <k-sintu@hotmail.com>
2016-06-02 18:00:15 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -618,7 +618,7 @@ namespace Championship_Riven
                         if (Target.IsValidTarget(200))
                         {
                            Core.DelayAction( () => Player.CastSpell(SpellSlot.E, EPos),1200);
-                           Core.DelayAction( () => Player.CastSpell(SpellSlot.W), 1250);
+                           Core.DelayAction( () => Player.CastSpell(SpellSlot.W), 1210);
                         }
 
                     }
