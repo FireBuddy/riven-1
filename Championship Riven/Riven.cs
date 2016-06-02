4f5eb02a6b407e72856ed2a6b5e025888820377b
AkumaCORE <k-sintu@hotmail.com>
2016-06-02 11:47:45 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -583,7 +583,7 @@ namespace Championship_Riven
             {
                 if(Q.IsReady())
                 {
-                    if(Minion.IsValidTarget(Q.Range))
+                    if(Minion.IsValidTarget(Q.Range) && Orbwalker.LastTarget.NetworkId != Minion.NetworkId)
                     {
                         if(Minion.Health - SpellQDamage(Minion, Minion.Health) <= 0)
                         {
