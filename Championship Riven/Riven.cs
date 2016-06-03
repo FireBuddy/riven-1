13be984b4aed75d3b16fa3d9c59964b9a1176641
AkumaCORE <k-sintu@hotmail.com>
2016-06-03 10:12:49 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -79,7 +79,7 @@ namespace Championship_Riven
 
         public static void Obj_AI_Turret_OnBasicAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
-            if (sender is Obj_AI_Turret && !args.Target is AIHeroClient && sender.IsAlly)
+            if (sender is Obj_AI_Turret && !args.Target is AIHeroClient && Target.IsEnemy)
             {
                 Chat.Print("A Turret is ATTACKING minion!");
             }
