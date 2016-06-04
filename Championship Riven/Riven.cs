4a57fd278f8c7405c84772ca1b9f0986c4ab822f
AkumaCORE <k-sintu@hotmail.com>
2016-06-04 11:19:04 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -1041,7 +1041,7 @@ namespace Championship_Riven
                 
                 Core.DelayAction(() =>
                 {
-                    if (!Player.Instance.IsRecalling() && CountQ <= 2 && minions.Count <= 0)
+                    if (!Player.Instance.IsRecalling() && CountQ <= 2 )
                     {
                         Player.CastSpell(SpellSlot.Q,
                             Orbwalker.LastTarget != null && Orbwalker.LastAutoAttack - Environment.TickCount < 3000
