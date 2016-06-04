d5c09e62642fe54d182e57df608a41e91f918dbe
AkumaCORE <k-sintu@hotmail.com>
2016-06-04 09:51:45 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -84,7 +84,7 @@ namespace Championship_Riven
                 
                 if (!(args.Target is AIHeroClient))
                 {
-                    var Minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.Instance.Position, 450);
+                    var Minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy,EntityManager.UnitTypes.Ranged, Player.Instance.Position, 450);
                     foreach (var Minion in Minions)
                     if(Minion.Health > Player.Instance.TotalAttackDamage && Minion.Health - sender.TotalAttackDamage * 1 <= 0)
                     
