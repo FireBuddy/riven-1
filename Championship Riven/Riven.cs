203bd676b19df825c5b18403e006583fbae0d099
AkumaCORE <k-sintu@hotmail.com>
2016-06-04 15:17:30 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -759,28 +759,6 @@ namespace Championship_Riven
 
                     }
                 }
-                {
-
-                    if(Q.IsReady())
-                    {
-                        if(Minion.IsValidTarget(Q.Range * 2 + 125) && !Minion.IsDead )
-                        {
-    
-                            if(Player.Instance.IsFacing(Minion) && ObjectManager.Player.Position.Distance(Minion.ServerPosition) > 409 && Minion.Health - Player.Instance.TotalAttackDamage * 1.2 <= 0)
-                            {
-                               
-                                Q.Cast(Player.Instance.Position.Extend(Minion.ServerPosition, 200).To3D());
-                            }
-                            
-                        }
-                    }
-                    else if(E.IsReady() && CountQ > 1 && Minion.IsValidTarget(E.Range + 125) && ObjectManager.Player.Position.Distance(Minion.ServerPosition) > 300  && Minion.Health - SpellQDamage(Minion, Minion.Health) * 2 <= 0 )
-                    {
-                          E.Cast(Player.Instance.Position.Extend(Minion.ServerPosition, 200).To3D());
-                    }      
-                }
-                
-        
         }
 
         
