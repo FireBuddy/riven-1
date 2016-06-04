4ae217c6f9c71e371b06ac202ec517014d4c9a6d
AkumaCORE <k-sintu@hotmail.com>
2016-06-04 11:51:01 -0400

    Update Riven.cs

--- Championship Riven/Riven.cs
+++ Championship Riven/Riven.cs
@@ -88,7 +88,7 @@ namespace Championship_Riven
                     var Minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.Instance.Position, 450);
                     foreach (var Minion in Minions)
                     
-                    if(Minion.Health > Player.Instance.TotalAttackDamage && Minion.Health - sender.TotalAttackDamage * 1.3 <= 0 )
+                    if(args.Target.Health > Player.Instance.TotalAttackDamage && Minion.Health - sender.TotalAttackDamage * 1.3 <= 0 )
                      
                     {
                         if( Minion.IsValidTarget(200) && !Minion.IsDead && Minion == args.Target )
